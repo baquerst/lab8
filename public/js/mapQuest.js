193922a3961c4ba07ee0ee1b40c7ab62f1f90a9d
Bryan Lee <blee379@gmail.com>
2020-02-26 19:26:53 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'PpuOtN4BHjAW4co2jdzLstl2R94kF0MW';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8788033, -117.2359121],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//Add a marker to the map
+	L.marker([32.8788033, -117.2359121]).addTo(map);
 }
